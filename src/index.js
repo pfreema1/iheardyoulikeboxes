67317b9067acb2f9615add12edb0064a570d608a
pfreema1 <pfreema1@gmail.com>
2018-11-01 20:03:17 -0400

    sound on boxes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 var container;
 var camera, scene, renderer;
-var uniforms, material, mesh;
+var uniforms, uniforms1, material, material1, mesh;
 var scale = 0.0;
 var mouseX = 0, mouseY = 0,
 lat = 0, lon = 0, phy = 0, theta = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-var cube;  
+var cube, cube1;  
 var startTime = Date.now();
+var geometry, geometry1;
+var synth;
+var tweenController = new TweenController();
 
 init();
 animate();
@@ -16,6 +19,7 @@ function init() {
   container = document.getElementById( 'container' );
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
   scene = new THREE.Scene();
+
   
   
   uniforms = {
@@ -24,33 +28,59 @@ function init() {
     scale: { type: "f", value: 1.0 },
   };
   
-  var geometry = new THREE.BoxBufferGeometry(10, 10, 10);
+  uniforms1 = {
+    time: { value: 1.0 }
+  };
+  
+  geometry = new THREE.BoxBufferGeometry(20, 20, 20);
   material = new THREE.ShaderMaterial( {
     uniforms: uniforms,
     vertexShader: document.getElementById( 'vertexShader' ).textContent,
     fragmentShader: document.getElementById( 'fragmentShader' ).textContent
   });
-  //material = new THREE.MeshBasicMaterial();
   cube = new THREE.Mesh( geometry, material );
   cube.position.set(0, 0, 0);
   scene.add( cube );
-
+  
+  geometry1 = new THREE.BoxBufferGeometry(20, 20, 20);
+  material1 = new THREE.ShaderMaterial({
+    uniforms: uniforms1,
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'fragment_shader4' ).textContent
+  });
+  cube1 = new THREE.Mesh(geometry, material);
+  cube1.position.set(30, 5, 0);     
+  scene.add(cube1);
+  
+  // right cube tween
+  let tween = new TweenMax.fromTo(cube1.rotation, 5, {
+    y: '-0.4'
+  },{
+    y: '0.1'
+  });
+  cube1.tl = new TimelineMax({
+    yoyo: true,
+    repeat: -1,
+    ease: Power2.easeInOut,
+  });
+  cube1.tl.add(tween);
+  
   camera.position.z = 50;
-
-  renderer = new THREE.WebGLRenderer();
+  
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio( window.devicePixelRatio );
   
   container.appendChild( renderer.domElement );
   
   uniforms.resolution.value.x = window.innerWidth;
   uniforms.resolution.value.y = window.innerHeight;
-
-  //camera.position.z = 1000;
-  //camera.lookAt(cube.position);
   
-
+  initializeTone();
   listeners();
-
+  
 }
 
 function animate() {
@@ -61,20 +91,27 @@ function animate() {
 function render() {
   var elapsedMilliseconds = Date.now() - startTime;
   var elapsedSeconds = elapsedMilliseconds / 1000.;
+
   uniforms.time.value = 60. * elapsedSeconds;
   uniforms.scale.value = scale;
 
+  uniforms1.time.value += 60. * elapsedSeconds;
+
   cube.rotation.y += 0.005;
   cube.rotation.x += 0.003;
+
+  // if(scale > 0.2) scale -= 0.04;
   
-  if(scale > 1.0) scale -= 0.07;
-  
-  if(scale < 0.5) scale = 0.5;
+  // if(scale < 0.1) scale = 0.1;
   
   renderer.render( scene, camera );
 }
 
+function initializeTone() {
+  synth = new Tone.Synth().toMaster();
 
+  
+}
 
 function listeners() {
   document.body.onkeyup = function(e) {
@@ -85,6 +122,15 @@ function listeners() {
 }
 
 function handleSpacePress() {
-  scale = 3.0;
-  console.log('hi');  
+  // visual
+  scale = 1.0;
+
+  // aural
+  // play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C2', '8n');
+  
+}
+
+function TweenController() {
+  this.currTween = null;
 }
